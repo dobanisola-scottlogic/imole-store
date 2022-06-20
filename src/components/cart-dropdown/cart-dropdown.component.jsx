bfb37bf450b8cf99c13a142acd622154e7e46025
Deborah Obanisola <dobanisola@scottlogic.com>
2022-06-20 14:17:29 +0100

    created checkout page, along side with checkout item and implemented the complete checkout functionality

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { CartContext } from '../../contexts/cart.context';
 
 import Button from '../button/button.component';
@@ -8,6 +9,11 @@ import './cart-dropdown.styles.scss';
 
 const CartDropdown = () =>{
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckout = () => {
+    navigate('/checkout');
+  }
 
   return (
     <div className="cart-dropdown-container">
@@ -20,7 +26,7 @@ const CartDropdown = () =>{
         <span className='empty-message'>Your cart is empty</span>
       )}
       </div>
-      <Button>GO TO CHECKOUT</Button>
+      <Button type="button" onClick={goToCheckout}>GO TO CHECKOUT</Button>
     </div>
   );
 };
---
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -0,0 +1,39 @@
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
+import "./checkout-item.styles.scss";
+
+const CheckoutItem = ({ cartItem  }) => {
+  const { imageUrl, price, name, quantity } = cartItem;
+  const {
+    addItemToCart,
+    reduceItemFromCart,
+    removeProductFromCart,
+  } = useContext(CartContext);
+
+  const addItem = () => {
+    addItemToCart(cartItem);
+  }
+  const reduceItem = () => {
+    reduceItemFromCart(cartItem);
+  }
+  const removeItem = () => {
+    removeProductFromCart(cartItem);
+  }
+  return (
+    <div className="checkout-item-container">
+    <div className="image-container">
+      <img src={imageUrl} alt={`${name}`} />
+      </div>
+      <span className="item-text name">{name}</span>
+      <span className="item-text quantity">
+        <div className="arrow" onClick={addItem}>&#10094;</div> 
+        <span className="value">{quantity}</span> 
+        <div className="arrow" onClick={addItem}>&#10095;</div>
+      </span>
+      <span className="item-text price">{price}</span>
+      <div className="remove-button" onClick={removeItem}>&#10005;</div>
+    </div>
+  );
+};
+
+export default CheckoutItem;
